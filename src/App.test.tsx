3000111085c77eb4fca598bdd8f36c3642f04595
Dylan Brunning <29053913+drbrunning@users.noreply.github.com>
2024-02-27 11:45:09 -0500

    Jest configuration for testing.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
-test('renders learn react link', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+// Mock the store
+
+test('renders login page.', () => {
+    render(
+        <Provider store={store}>
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        </Provider>
+    );
+    const linkElement = screen.getByText(/login/i);
     expect(linkElement).toBeInTheDocument();
 });
