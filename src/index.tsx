0a262d5a26806be90642084ad71c752a106e863d
Dylan Brunning <29053913+drbrunning@users.noreply.github.com>
2024-02-26 20:49:07 -0500

    Spacing fix.

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ import './input.css';
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
 root.render(
     <React.StrictMode>
         <Provider store={store}>
